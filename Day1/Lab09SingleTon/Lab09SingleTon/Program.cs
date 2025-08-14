6e93b1152cb276569ed9265fdeb6c01f1fd486f2
kannan <kannans89@hotmail.com>
2025-08-14 12:51:16 +0530

    singleton

---
+++ Day1/Lab09SingleTon/Lab09SingleTon/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+using Lab09SingleTon;
+
+ServiceRegistry reg1 =  ServiceRegistry.CreateInstance();
+ServiceRegistry reg2 =  ServiceRegistry.CreateInstance();
+
+reg1.DoLookup();
+reg2.DoLookup();
+
+Console.WriteLine(reg1.GetHashCode()) ;
+Console.WriteLine(reg2.GetHashCode());
---
+++ Day1/Lab09SingleTon/Lab09SingleTon/ServiceRegistry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09SingleTon
+{
+    internal class ServiceRegistry
+    {
+        private static ServiceRegistry _myInstance=null;
+        private ServiceRegistry() {
+
+            Console.WriteLine("Service regsitry created");
+        
+        }
+
+        public static ServiceRegistry CreateInstance() { 
+        
+            if(_myInstance == null) {
+                _myInstance = new ServiceRegistry();
+            }
+            return _myInstance;
+
+        }
+        public void DoLookup() {
+            Console.WriteLine("Doing lookup in service registry"+this.GetHashCode());
+        }
+    }
+
+}
